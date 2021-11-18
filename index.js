5285bdb6f202c9fa7bd87e195c707b43fe852e6b
zaklinaradivojevic <zaklinaradivojevic77777777@gmail.com>
2021-11-18 18:26:16 +0100

    updeted changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-$(document).ready(function () {
-    $('button').click(function () {
-        $('ul').toggleClass('show');
-        console.log("button has been clicked");
-    });
-});
+function openMenu() {
+    document.getElementById("toggle").classList.toggle("show");
+    document.getElementById("close").style.display = "block";
+}
+function closeMenu() {
+    document.getElementById("toggle").classList.remove("show");
+    document.getElementById("close").style.display = "none";
+
+}
